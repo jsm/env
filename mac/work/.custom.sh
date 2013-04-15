6249c8e691ad2cec34dbb886c40f98bffbb11658
Jon San Miguel <jon@serv.io>
2013-04-15 11:48:06 -0700

    Macports PATH work work comp

--- mac/work/.custom.sh
+++ mac/work/.custom.sh
@@ -1,3 +1,5 @@
+export PATH=/opt/local/bin:/opt/local/sbin:$PATH
+
 alias e="/Applications/MacPorts/Emacs.app/Contents/MacOS/Emacs -nw"
 alias emacs="/Applications/MacPorts/Emacs.app/Contents/MacOS/Emacs"
 alias mysql=mysql5
