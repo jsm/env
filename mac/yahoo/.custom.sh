9a9d183328271b9516e591134f64148e355ec5ac
Jon San Miguel <sanmiguelje@yahoo.com>
2013-08-13 14:35:58 -0700

    Added yahoo profile for mac

---
+++ mac/yahoo/.custom.sh
@@ -0,0 +1,27 @@
+export PATH=/Users/jsm/Library/Python/2.7/bin:$PATH
+
+alias e="emacs -nw"
+alias rv="ruby -e 'print RUBY_VERSION'"
+
+db_deploy () {
+    bundle exec rake db:reset
+    bundle exec rake db:populate
+    bundle exec rake db:test:prepare
+}
+db_migrate() {
+    bundle exec rake db:migrate
+    bundle exec rake db:test:prepare
+}
+
+function _update_ps1() {
+    export PS1="$(~/.powerline-shell.py $?)"
+}
+
+alias ccat="/bin/cat"
+alias cat="pygmentize -g"
+
+export PROMPT_COMMAND="_update_ps1"
+export CATALINA_HOME="/opt/boxen/homebrew/Cellar/tomcat/7.0.41"
+export CATALINA_OPTS="$CATALINA_OPTS -Drun.env=stage"
+
+source /opt/boxen/env.sh
