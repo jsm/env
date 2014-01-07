6c470bf2630e270998387bbb3fb46e18c0b43303
Jon San Miguel <sanmiguelje@yahoo.com>
2014-01-06 16:04:21 -0800

    Add mac opti profile

---
+++ mac/opti/.custom.sh
@@ -0,0 +1,36 @@
+alias e="emacs -nw"
+
+db_deploy () {
+    bundle exec rake db:reset
+    bundle exec rake db:populate
+    bundle exec rake db:test:prepare
+}
+db_migrate() {
+    bundle exec rake db:migrate
+    bundle exec rake db:test:prepare
+}
+
+function _update_ps1() {
+    export PS1="$(~/.powerline-shell.py $?)"
+    echo -ne "\\033]0;${PWD##*/}\\007"
+}
+
+alias ccat=/bin/cat
+alias cat="pygmentize -g"
+alias cdo="cd /opti"
+
+export PROMPT_COMMAND="_update_ps1"
+
+### Added by the Heroku Toolbelt
+export PATH="/usr/local/heroku/bin:$PATH"
+
+export PATH=/opt/chef/embedded/bin:$PATH
+
+export PATH=/Users/jsm/Library/Python/2.7/bin:$PATH
+
+export PATH=/usr/local/bin:$PATH
+export PATH=/usr/local/sbin:$PATH
+export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
+
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+[[ -s "$HOME/.rvm/scripts/cd" ]] && source "$HOME/.rvm/scripts/cd"
