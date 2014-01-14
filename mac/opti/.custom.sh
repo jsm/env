2e60ba13d37508aa73bfb8b54391f90480a722b5
Jon San Miguel <sanmiguelje@yahoo.com>
2014-01-14 11:50:18 -0800

    Update mac/opti paths

--- mac/opti/.custom.sh
+++ mac/opti/.custom.sh
@@ -36,6 +36,9 @@ export PATH=/opti/optimizely/out:$PATH
 export PATH=/opti/optimizely/out/ec2_api_tools/bin:$PATH
 export EC2_HOME=/opti/optimizely/out/ec2_api_tools
 export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home
+export PATH=/opti/optimizely2/out:$PATH
+export PATH=/opti/optimizely2/out/ec2_api_tools/bin:$PATH
+export EC2_HOME=/opti/optimizely2/out/ec2_api_tools
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 [[ -s "$HOME/.rvm/scripts/cd" ]] && source "$HOME/.rvm/scripts/cd"
