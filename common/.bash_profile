c99a157a632ed65c336b1d783196f4405c75831a
Jon San Miguel <sanmiguelje@yahoo.com>
2014-01-14 17:01:36 -0800

    Update mac opti

--- common/.bash_profile
+++ common/.bash_profile
@@ -1,1 +1,8 @@
 source ~/.bashrc
+export PATH=/opti/optimizely2/out:$PATH
+export PATH=/opti/optimizely2/out/ec2_api_tools/bin:$PATH
+export EC2_HOME=/opti/optimizely2/out/ec2_api_tools
+export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home
+export PATH=/opti/optimizely/out:$PATH
+export PATH=/opti/optimizely/out/ec2_api_tools/bin:$PATH
+export EC2_HOME=/opti/optimizely/out/ec2_api_tools
--- mac/opti/.custom.sh
+++ mac/opti/.custom.sh
@@ -18,6 +18,7 @@ function _update_ps1() {
 alias ccat=/bin/cat
 alias cat="pygmentize -g"
 alias cdo="cd /opti"
+alias cdopt="cd /opti/optimizely"
 
 export PROMPT_COMMAND="_update_ps1"
 
@@ -32,13 +33,10 @@ export PATH=/usr/local/bin:$PATH
 export PATH=/usr/local/sbin:$PATH
 export PATH=$PATH:$HOME/.rvm/bin # Add RVM to PATH for scripting
 
+export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home
 export PATH=/opti/optimizely/out:$PATH
 export PATH=/opti/optimizely/out/ec2_api_tools/bin:$PATH
 export EC2_HOME=/opti/optimizely/out/ec2_api_tools
-export JAVA_HOME=/System/Library/Frameworks/JavaVM.framework/Home
-export PATH=/opti/optimizely2/out:$PATH
-export PATH=/opti/optimizely2/out/ec2_api_tools/bin:$PATH
-export EC2_HOME=/opti/optimizely2/out/ec2_api_tools
 
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 [[ -s "$HOME/.rvm/scripts/cd" ]] && source "$HOME/.rvm/scripts/cd"
